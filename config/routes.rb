b51fc2392c962a09734b2cc1eee1cbc686621162
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-05-12 18:48:22 +0000

    Generate Topic resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
 
+  # Routes for the Topic resource:
+
+  # CREATE
+  post("/insert_topic", { :controller => "topics", :action => "create" })
+          
+  # READ
+  get("/topics", { :controller => "topics", :action => "index" })
+  
+  get("/topics/:path_id", { :controller => "topics", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_topic/:path_id", { :controller => "topics", :action => "update" })
+  
+  # DELETE
+  get("/delete_topic/:path_id", { :controller => "topics", :action => "destroy" })
+
+  #------------------------------
+
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
